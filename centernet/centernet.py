359b8f01b45d2ba3768453e69bc88f8e391f020b
a5chin <capricorn42551230@gmail.com>
2022-06-27 00:09:14 +0900

    create losses

--- centernet/centernet.py
+++ centernet/centernet.py
@@ -4,6 +4,7 @@ import torch
 from torch import nn
 
 from .backbone import resnet18
+from .losses import GaussianFocalLoss, L1Loss
 from .modules import CenterNetHead, CTResNetNeck
 
 
@@ -17,6 +18,10 @@ class CenterNet(nn.Module):
             num_deconv_kernels=(4, 4, 4),
         )
         self.bbox_head = CenterNetHead(in_channels=64, feat_channels=64, num_classes=4)
+
+        self.loss_center_haetmap = GaussianFocalLoss(loss_weight=1.0)
+        self.loss_wh = L1Loss(loss_weight=0.1)
+        self.loss_offset = L1Loss(loss_weight=1.0)
 
     def forward(self, x: torch.Tensor) -> Dict[str, torch.Tensor]:
         x = self.backbone(x)
